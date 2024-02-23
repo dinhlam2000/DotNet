200170f2f7f28c3695211591bd48c36bd969caf0
Lam Tran <lamdtran2000@gmail.com>
2024-02-23 15:29:01 -0800

    beginner api done

--- obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b772f894bd9b95adf2bf777d8e98c35c7a55cf28")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
